58bccdb4bac10975635e384dc809381473420555
ALUNO C25 <alunoc25@unifor.local>
2025-10-14 19:31:08 -0300

    Commit inicial

---
+++ src/test/java/Api_Trabalho/Api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Api_Trabalho.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
